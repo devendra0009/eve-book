3402f09fb565d3530a04b8d72473db6621aed79e
devendra0009 <devendrabedwal4@gmail.com>
2026-05-23 12:56:21 +0530

    removed firebase config

--- src/main/java/com/davendra/event_booking/infra/firebase/FirebaseConfig.java
+++ src/main/java/com/davendra/event_booking/infra/firebase/FirebaseConfig.java
@@ -6,8 +6,10 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
@@ -28,24 +30,67 @@ public class FirebaseConfig {
 //        }
 //    }
 
+//
+//    @PostConstruct
+//    public void initialize() {
+//
+//        try {
+//
+//            // this is necessary to create firebase instance to call it from backend
+//
+//            InputStream serviceAccount = getClass()
+//                    .getClassLoader()
+//                    .getResourceAsStream("firebase/firebase_config.json");
+//
+//            if (serviceAccount == null) {
+//                throw new RuntimeException("Firebase service account file not found");
+//            }
+//
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+//
+//            if (FirebaseApp.getApps().isEmpty()) {
+//                FirebaseApp.initializeApp(options);
+//            }
+//
+//            System.out.println("Firebase initialized successfully");
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed to initialize Firebase", e);
+//        }
+//    }
+
 
     @PostConstruct
     public void initialize() {
 
         try {
 
-            // this is necessary to create firebase instance to call it from backend
+            // Read Base64 encoded Firebase config from ENV
+            String firebaseConfigBase64 =
+                    System.getenv("FIREBASE_CONFIG_BASE64");
 
-            InputStream serviceAccount = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream("firebase/firebase_config.json");
+            if (firebaseConfigBase64 == null ||
+                    firebaseConfigBase64.isBlank()) {
 
-            if (serviceAccount == null) {
-                throw new RuntimeException("Firebase service account file not found");
+                throw new RuntimeException(
+                        "FIREBASE_CONFIG_BASE64 env variable is missing"
+                );
             }
 
+            // Decode Base64 string
+            byte[] decodedBytes =
+                    Base64.getDecoder().decode(firebaseConfigBase64);
+
+            // Convert to InputStream
+            InputStream serviceAccount =
+                    new ByteArrayInputStream(decodedBytes);
+
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(
+                            GoogleCredentials.fromStream(serviceAccount)
+                    )
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
@@ -55,7 +100,13 @@ public class FirebaseConfig {
             System.out.println("Firebase initialized successfully");
 
         } catch (Exception e) {
-            throw new RuntimeException("Failed to initialize Firebase", e);
+
+            e.printStackTrace();
+
+            throw new RuntimeException(
+                    "Failed to initialize Firebase",
+                    e
+            );
         }
     }
 }
